2f329bfef7d35992cfc476862deb3306fd542ee5
Ilia Obukhov <proorc9@gmail.com>
2021-12-08 00:27:19 +0300

    Initial commit

--- build/install.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-root = os.path.dirname(__file__)
-pythonpath = os.path.join(root, "python")
-sys.path.insert(0, pythonpath)
-
-from rezutil import _rezbuild
-_rezbuild.main([root])
